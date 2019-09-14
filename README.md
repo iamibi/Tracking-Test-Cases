215fa52a87a5597b43b0881f83ee83e44594e6fb
iamibi <ibrahim@poshmark.com>
2019-09-14 21:57:57 +0530

    Added code for reading a feature file and printing it

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This repository will contain my project which helps in tracking test cases.
 It's a work in progress
 
 # TODO
-1. Read data from files.
+1. Read data from files. 
 2. Create a high level structure of JSON.
 3. Check the activity status based on the changes in the test case.
--- src/init_module.py
+++ src/init_module.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+
+
+def main():
+	# Read file
+	filename = 'sample.feature'
+	path_to_file = 'Tracking-Test-Cases/data/'
+
+	if not os.path.exists(path_to_file):
+		print("The path doesn't exist")
+		return
+
+	os.chdir(path_to_file)
+
+	try:
+		with open(filename) as test_file:
+			for line_count, line in enumerate(test_file):
+				stripped_line = line.strip('\n \t')
+				if stripped_line != '':
+					print("Line {}: {}".format(line_count, stripped_line))
+	except IOError as error:
+		print("An error occurred: {}".format(str(error)))
+
 
 if __name__ == '__main__':
-	print("Hello world")
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("Exiting the system")
+		sys.exit(0)
